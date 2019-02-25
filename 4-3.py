c9a4c1fc1bde16d634da0303c01d115a086796f8
LJZSR <jzs1996@sjtu.edu.cn>
2019-02-25 16:18:53 +0800

    Update 4-3.py

--- 4-3.py
+++ 4-3.py
@@ -4,6 +4,10 @@ b = tf.placeholder(tf.int16)
 add = tf.add(a, b)
 mul = tf.multiply(a, b)
 with tf.Session() as sess:
+    
     #计算具体数值
+    #feed机制
     print("相加：%i" % sess.run(add, feed_dict={a: 3, b: 4}))
     print("相乘：%i" % sess.run(mul, feed_dict={a: 3, b: 4}))
+    #fetch机制
+    print(sess.run([mul, add], feed_dict={a: 3, b: 4}))
