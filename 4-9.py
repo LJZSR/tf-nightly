52b3a699206fffc9003bcca8d376e514713cefc6
LJZSR <jzs1996@sjtu.edu.cn>
2019-03-01 18:14:11 +0800

    Update 4-9.py

--- 4-9.py
+++ 4-9.py
@@ -14,3 +14,12 @@ with tf.Session() as sess:
     print('var1=', var1.eval())
     print('var2=', var2.eval())
 
+get_var1 = tf.get_variable('firstvar', [1], initializer=tf.constant_initializer(0.3))
+print('get_var1:', get_var1.name)
+
+get_var1 = tf.get_variable('firstvar1', [1], initializer=tf.constant_initializer(0.4))
+print('get_var1:', get_var1.name)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    print('get_var1=', get_var1.eval())
