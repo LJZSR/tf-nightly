bba55f0c1f42b0eebca37df3c7ab5a94df414583
LJZSR <jzs1996@sjtu.edu.cn>
2019-03-06 10:53:49 +0800

    Update 6-1.py

--- 6-1.py
+++ 6-1.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 
+#标签为标准one_hot向量
 labels = [[0,0,1],[0,1,0]]
 logits = [[2,0.5,6],[0.1,0,3]]
 logits_scaled = tf.nn.softmax(logits)
@@ -18,3 +19,17 @@ with tf.Session() as sess:
     print('rel2=', sess.run(result2))
     print('rel3=', sess.run(result3))
     print('rel4=', sess.run(result4))
+
+#标签总概率为1
+labels2 = [[0.4,0.1,0.5],[0.3,0.6,0.1]]
+result5 = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels2)
+
+with tf.Session() as sess:
+    print('rel5=', sess.run(result5))
+
+labels3 = [[2,0.5,6],[0.1,0,3]]
+result6 = tf.log(logits_scaled)
+
+with tf.Session() as sess:
+    print('rel6=', sess.run(result6))
+    print('rel7=', sess.run(labels*result6))
